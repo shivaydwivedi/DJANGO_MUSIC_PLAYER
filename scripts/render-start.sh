d4001e52be1298097c8131f5a2f6b33ae9e293ac
shivaydwivedi <polymath1510@gmail.com>
2026-07-28 22:54:04 +0530

    fix: trust Render HTTPS proxy headers

--- scripts/render-start.sh
+++ scripts/render-start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-exec waitress-serve --listen="0.0.0.0:${PORT:-10000}" musicplayer.wsgi:application
+exec waitress-serve \
+  --listen="0.0.0.0:${PORT:-10000}" \
+  --trusted-proxy="*" \
+  --trusted-proxy-headers="x-forwarded-proto" \
+  musicplayer.wsgi:application
